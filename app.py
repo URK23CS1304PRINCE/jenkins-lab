f43a4bbba7c6c6e2a7e5e331c045f3f62ecb8e08
URK23CS1304PRINCE <153969784+URK23CS1304PRINCE@users.noreply.github.com>
2026-04-20 16:47:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
-app=Flask(__name__)
+app = Flask(__name__)
 @app.route('/')
-def home(): return "Hello Jenkins"
-app.run(host='0.0.0.0')
+     def home():
+        return "Hello Jenkins CI"
+app.run(host = '0.0.0.0', port = 5000)
